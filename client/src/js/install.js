171f2292560499bd70f3705652eba3d7bba5a496
Ross Graves <ross@workwithcanary.com>
2022-10-09 00:48:44 -0600

    JATE PWA

---
+++ client/src/js/install.js
@@ -0,0 +1,27 @@
+const butInstall = document.getElementById('buttonInstall');
+
+let deferredPrompt;
+
+// Logic for installing the PWA
+// TODO: Add an event handler to the `beforeinstallprompt` event
+window.addEventListener('beforeinstallprompt', (event) => {
+	e.preventDefault();
+	deferredPrompt = e;
+	butInstall.classList.toggle('hidden', false);
+});
+
+// TODO: Implement a click event handler on the `butInstall` element
+butInstall.addEventListener('click', async () => {
+	if(!deferredPrompt)
+		return
+
+	deferredPrompt.prompt();
+	deferredPrompt = null;
+
+	butInstall.classList.toggle('hidden', true);
+});
+
+// TODO: Add an handler for the `appinstalled` event
+window.addEventListener('appinstalled', (event) => {
+	deferredPrompt - null;
+});
---
+++ client/webpack.config.js
@@ -0,0 +1,65 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const WebpackPwaManifest = require('webpack-pwa-manifest');
+const path = require('path');
+const { InjectManifest } = require('workbox-webpack-plugin');
+
+// TODO: Add and configure workbox plugins for a service worker and manifest file.
+// TODO: Add CSS loaders and babel to webpack.
+
+module.exports = () => {
+  return {
+    mode: 'development',
+    entry: {
+      main: './src/js/index.js',
+      install: './src/js/install.js'
+    },
+    output: {
+      filename: '[name].bundle.js',
+      path: path.resolve(__dirname, 'dist'),
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+       title: 'J.A.T.E.',
+       inject: true,
+       template: './index.html',
+      }),
+      new WebpackPwaManifest({
+        filename: "mainfest.json",
+        display: "standalone",
+        inject: true,
+        name: "J.A.T.E",
+        crossorigin: null,
+        start_url: "/",
+        public_path: "/",
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          },
+        ],
+      }),
+      new InjectManifest({
+        swSrc: './src-sw.js',
+      }),
+    ],
+    module: {
+      rules: [
+        {
+          test: /\.m?js$/,
+          exclude: /(node_modules|bower_components)/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env']
+            }
+          }
+        },
+        {
+          test: /\.css$/i,
+          use: ["style-loader", "css-loader"]
+        }
+      ],
+    },
+  };
+};
